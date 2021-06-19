ba1cd8f95dd90ca25b23bf782747c42aad516fb4
CHIRAG3899 <66902259+CHIRAG3899@users.noreply.github.com>
2021-06-19 13:33:11 +0530

    Add files via upload

---
+++ June Leetcode Challenge 2021/19 K Inverse Pairs Array.py
@@ -0,0 +1,27 @@
+#For an integer array nums, an inverse pair is a pair of integers [i, j] where
+#0 <= i < j < nums.length and nums[i] > nums[j].
+#Given two integers n and k, return the number of different arrays consist of
+#numbers from 1 to n such that there are exactly k inverse pairs. Since the
+#answer can be huge, return it modulo 109 + 7.
+#Example 1:
+#Input: n = 3, k = 0
+#Output: 1
+#Explanation: Only the array [1,2,3] which consists of numbers from 1 to 3 has
+#exactly 0 inverse pairs.
+#Example 2:
+#Input: n = 3, k = 1
+#Output: 2
+#Explanation: The array [1,3,2] and [2,1,3] have exactly 1 inverse pair.
+
+class Solution:
+    def kInversePairs(self, n, k):
+        dp = [[1] * (k+1) for _ in range(n+1)]
+        sp = [[1] * (k+1) for _ in range(n+1)]
+        N = 10**9 + 7
+
+        for i in range(1, n+1):
+            for j in range(1, k+1):
+                dp[i][j] = sp[i-1][j] if j < i else (sp[i-1][j] - sp[i-1][j-i]) % N
+                sp[i][j] = (sp[i][j-1] + dp[i][j]) % N
+        
+        return dp[-1][-1]
